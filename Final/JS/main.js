309a279c83a5c4bc7de1e7a7485e3dd29d9feb2c
Anthony Raponi <aj.raponi@nyu.edu>
2017-12-12 18:29:12 -0500

    Final Project

---
+++ Final/JS/main.js
@@ -0,0 +1,23 @@
+// $("#test").on('click', function() {
+//
+//   $.fancybox.open([
+//     {
+//       src  : 'https://c1.staticflickr.com/9/8148/29324593462_abebaddc38_k.jpg',
+//       opts : {
+//         caption : 'First caption',
+//         thumb   : 'https://c1.staticflickr.com/9/8148/29324593462_f890687b7a_m.jpg'
+//       }
+//     },
+//     {
+//       src  : 'https://c2.staticflickr.com/6/5499/30972532232_051e1dc57e_h.jpg',
+//       opts : {
+//         caption : 'Second caption',
+//         thumb   : 'https://c2.staticflickr.com/6/5499/30972532232_e9a298a0c5_m.jpg'
+//       }
+//     }
+//   ], {
+//     loop : true,
+//     hash : "test"
+//   });
+//
+// });
